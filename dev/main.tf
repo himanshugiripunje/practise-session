745c4a99811096afd517bb1d201d6c08756820e5
smithgiripunje <smithgiripunje@gmail.com>
2023-03-08 19:05:29 +0530

    auto-commit

--- dev/main.tf
+++ dev/main.tf
@@ -12,5 +12,4 @@ module "vpc" {
   public_subnet_cidr_block  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnet_cidr_block = ["10.0.3.0/24", "10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
   availability_zones        = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
-
 }
